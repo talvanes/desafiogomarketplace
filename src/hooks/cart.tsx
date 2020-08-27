bf0a5dd8be30668d83b270abd8765c71eace33c2
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-27 12:51:07 -0300

    GoMarketplace Should Load Products from AsyncStorage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,14 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      // LOAD ITEMS FROM ASYNC STORAGE
+      const [cart] = await AsyncStorage.multiGet(['@GoMarketplace:cart']);
+
+      const storagedProducts = cart[1]
+        ? (JSON.parse(cart[1]) as Product[])
+        : [];
+
+      setProducts(storagedProducts);
     }
 
     loadProducts();
