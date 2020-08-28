77efa68fdf3c83c2835d2840276b25fbdc9d89e0
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-28 10:48:19 -0300

    Floating Cart Should Calculate Quantities and Totals

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    // RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const result = products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(result);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    // RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const result = products.reduce((sum, product) => sum + product.quantity, 0);
 
-    return 0;
+    return result;
   }, [products]);
 
   return (
