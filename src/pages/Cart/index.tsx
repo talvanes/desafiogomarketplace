a5404d03b3b23b98b0826016c5f5620492a63c63
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-28 10:39:53 -0300

    GoMarketplace Should Calculate Quantities and Totals

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,20 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    // TODO THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const result = products.reduce(
+      (total, product) => total + product.quantity * product.price,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(result);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    // RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const result = products.reduce((sum, product) => sum + product.quantity, 0);
 
-    return 0;
+    return result;
   }, [products]);
 
   return (
